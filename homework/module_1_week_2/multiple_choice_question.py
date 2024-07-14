9efdb7e23213e91b632a0a6c5aabb0fc6f72d594
Nguyen Thi Kim Tuyen <tuyen03a128@gmail.com>
2024-07-14 21:22:31 +0700

    Add homework module 2 - week 1

--- homework/module_1_week_2/multiple_choice_question.py
+++ homework/module_1_week_2/multiple_choice_question.py
@@ -2,38 +2,23 @@
 def max_kernel(num_list, k):
     result = []
 
-    # Your Code Here
     for i in range(len(num_list) - k + 1):
         window = num_list[i:i + k]
         result.append(max(window))
-    # End Code Here
     return result
-
-
-# assert max_kernel([3, 4, 5, 1, -44], 3) == [5, 5, 5]
-#
-# num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
-# k = 3
-# print(max_kernel(num_list, k))
 
 
 # Question 2
 def character_count(word):
     character_statistic = {}
 
-    # Your Code Here
     for char in word:
         if char in character_statistic:
             character_statistic[char] += 1
         else:
             character_statistic[char] = 1
 
-    # End Code Here
     return character_statistic
-
-
-# assert character_count("Baby") == {'B': 1, 'a': 1, 'b': 1, 'y': 1}
-# print(character_count('smiles'))
 
 
 # Question 3
@@ -54,17 +39,9 @@ def count_word(file_path):
     return counter
 
 
-# file_path = '/content/P1_data.txt'
-# file_path = 'P1_data.txt'
-# result = count_word(file_path)
-# assert result['who'] == 3
-# print(result['man'])
-
-
 #  Question 4
 
 def levenshtein_distance(token1, token2):
-    # Your Code Here
     if not token1:
         return len(token2)
     if not token2:
@@ -94,13 +71,8 @@ def levenshtein_distance(token1, token2):
 
     # The last element of the matrix is the Levenshtein distance.
     distance = distances[len(token1)][len(token2)]
-    # End Code Here
 
     return distance
-
-
-# assert levenshtein_distance("hi", "hello") == 4.0
-# print(levenshtein_distance("hola", "hello"))
 
 
 # Question 5
@@ -108,10 +80,8 @@ def check_the_number(N):
     list_of_numbers = []
     result = ""
     for i in range(1, 5):
-        # Your code here
         # Su dung append them i vao trong list_of_number
         list_of_numbers.append(i)
-        # End code here
     if N in list_of_numbers:
         results = " True "
     if N not in list_of_numbers:
@@ -119,18 +89,10 @@ def check_the_number(N):
     return results
 
 
-# N = 7
-# assert check_the_number(N) == " False "
-# N = 2
-# results = check_the_number(N)
-# print(results)
-
-
 # Question 6
 def my_function_6(data, max, min):
     result = []
     for i in data:
-        # Your code here
         # Neu i < min thi them min vao result
         if i < min:
             result.append(min)
@@ -141,73 +103,30 @@ def my_function_6(data, max, min):
     return result
 
 
-# my_list = [5, 2, 5, 0, 1]
-# max = 1
-# min = 0
-# assert my_function_6(max=max, min=min, data=my_list) == [1, 1, 1, 0, 1]
-# my_list = [10, 2, 5, 0, 1]
-# max = 2
-# min = 1
-# print(my_function_6(max=max, min=min, data=my_list))
-
-
 # Question 7
 
 def my_function_7(x, y):
-    # Your code here
     # Su dung extend de noi y vao x
     x.extend(y)
-    # End code here
     return x
-
-
-# list_num1 = ['a', 2, 5]
-# list_num2 = [1, 1]
-# list_num3 = [0, 0]
-#
-# assert my_function_7(list_num1, my_function_7(list_num2, list_num3)) == ['a', 2, 5, 1, 1, 0, 0]
-#
-# list_num1 = [1, 2]
-# list_num2 = [3, 4]
-# list_num3 = [0, 0]
-#
-# print(my_function_7(list_num1, my_function_7(list_num2, list_num3)))
 
 
 # Question 8
 def my_function_8(n):
-    # Your code here
     min_value = n[0]
     for num in n:
         if num < min_value:
             min_value = num
     return min_value
-    # End code here
-
-
-# my_list = [1, 22, 93, -100]
-# assert my_function_8(my_list) == -100
-#
-# my_list = [1, 2, 3, -1]
-# print(my_function_8(my_list))
 
 
 # Question 9
 def my_function_9(n):
-    # Your code here
     max_value = n[0]
     for num in n:
         if num > max_value:
             max_value = num
     return max_value
-    # End code here
-
-
-# my_list = [1001, 9, 100, 0]
-# assert my_function_9(my_list) == 1001
-#
-# my_list = [1, 9, 9, 0]
-# print(my_function_9(my_list))
 
 
 # Question 10
@@ -220,13 +139,6 @@ def My_function_10(integers, number=1):
     )
 
 
-# my_list = [1, 3, 9, 4]
-# assert My_function_10(my_list, -1) == False
-#
-# my_list = [1, 2, 3, 4]
-# print(My_function_10(my_list, 2))
-
-
 # Question 11
 def my_function_11(list_nums=[0, 1, 2]):
     var = 0
@@ -236,63 +148,37 @@ def my_function_11(list_nums=[0, 1, 2]):
         list_nums)  # Your code here : Tra ve gia tri trung binh cua list bang cach chia var cho so luong phan tu trong list_mums
 
 
-# assert my_function_11([4, 6, 8]) == 6
-# print(my_function_11())
-
-
 # Question 12
 def my_function_12(data):
     var = []
     for i in data:
-        # Your code here
         # Neu i chia het cho 3 thi them i vao list var
         if i % 3 == 0:
             var.append(i)
-    # End code here
     return var
-
-
-# assert my_function_12([3, 9, 4, 5]) == [3, 9]
-# print(my_function_12([1, 2, 3, 5, 6]))
 
 
 # Question 13
 def my_function_13(y):
     var = 1
     while (y > 1):
-        # Your code here
         var = var * y
         y -= 1
-    # End code here
     return var
-
-# assert my_function_13(8) == 40320
-# print(my_function_13(4))
 
 
 # Question 14
 def my_function_14(x):
-    # your code here
     return x[::-1]
-    # End code here
-
-
-# x = 'I can do it'
-# assert my_function_14(x) == "ti od nac I"
-#
-# x = 'apricot'
-# print(my_function_14(x))
 
 
 # Question 15
 def function_helper_15(x):
-    # Your code here
     # Neu x >0 tra ve 'T', nguoc lai tra ve 'N'
     if x > 0:
         return 'T'
     else:
         return 'N'
-    # End code here
 
 
 def my_function_15(data):
@@ -300,21 +186,12 @@ def my_function_15(data):
     return res
 
 
-# data = [10, 0, -10, -1]
-# assert my_function_15(data) == ['T', 'N', 'N', 'N']
-#
-# data = [2, 3, 5, -1]
-# print(my_function_15(data))
-
-
 # Question 16
 def function_helper_16(x, data):
     for i in data:
-        # Your code here
         # Neu x == i thi return 0
         if x == i:
             return 0
-    # End code here
     return 1
 
 
@@ -324,10 +201,3 @@ def my_function_16(data):
         if function_helper_16(i, res):
             res.append(i)
     return res
-
-
-# lst = [10, 10, 9, 7, 7]
-# assert my_function_16(lst) == [10, 9, 7]
-#
-# lst = [9, 9, 8, 1, 1]
-# print(my_function_16(lst))
